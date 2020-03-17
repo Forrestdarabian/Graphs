4c5444aa2af5604b8b656e20ca28106814235c15
Forrest-Darabian <mr.smiley72@yahoo.com>
2020-03-17 13:52:47 -0700

    using bfs to slove problem

--- projects/ancestor/ancestor.py
+++ projects/ancestor/ancestor.py
@@ -1,3 +1,20 @@
+class Queue():
+    def __init__(self):
+        self.queue = []
+
+    def enqueue(self, value):
+        self.queue.append(value)
+
+    def dequeue(self):
+        if self.size() > 0:
+            return self.queue.pop(0)
+        else:
+            return None
+
+    def size(self):
+        return len(self.queue)
+
+
 class Graph:
 
     """Represent a graph as a dictionary of vertices mapping labels to edges."""
@@ -25,15 +42,43 @@ class Graph:
 
 
 def earliest_ancestor(ancestors, starting_node):
+
     # make a graph
     graph = Graph()
+    # make a queue
+    queue = Queue()
 
     for pair in ancestors:
         # parent is the first pair
         parent = pair[0]
-        graph.add_vertex(pair[0])
+        graph.add_vertex(parent)
         # child is the second pair
         child = pair[1]
-        graph.add_vertex(pair[1])
+        graph.add_vertex(child)
         # add a directed edge
-        graph.add_edge(parent, child)
+        graph.add_edge(child, parent)
+
+    # enqueue the starting node since we want the longest path
+    queue.enqueue([starting_node])
+
+    longest_path_length = 0
+
+    # while the queue isnt empty, dequeue the next path
+    while queue.size() > 0:
+        path = queue.dequeue()
+        # current person is the last thing in the path
+        current_node = path[-1]
+
+        if len(path) > longest_path_length:
+            longest_path_length = len(path)
+
+        # using bfs, set neighbors equal to the current node set
+        neighbors = graph.vertices[current_node]
+        # create our for loop
+        for ancestor in neighbors:
+            # copy the path
+            path_copy = list(path)
+            # append the ancestor
+            path_copy.append(ancestor)
+            # enqueue the copied path
+            queue.enqueue(path_copy)
